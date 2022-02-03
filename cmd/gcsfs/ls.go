fbfa875dbb698164b02d929c3b06048b3454b866
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2022-02-03 10:00:56 -0300

    feat(cmd): add ls cmd

---
+++ cmd/gcsfs/ls.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+
+	"github.com/spf13/cobra"
+)
+
+var lsCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "List files",
+	Long:  "List files from a Google Storage Bucket",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := bucketSetupFunc(cmd, args)
+		if err != nil {
+			return err
+		}
+
+		if len(args) != 1 {
+			return cmd.Usage()
+		}
+
+		files, err := fs.ReadDir(GCSFS, args[0])
+		if err != nil {
+			return err
+		}
+
+		for _, file := range files {
+			fmt.Println(file.Name())
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lsCmd)
+}
